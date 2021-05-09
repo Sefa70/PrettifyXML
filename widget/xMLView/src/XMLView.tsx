1afa79c786551887830a338818a7786aa2f8637f
Sefa70 <Sefa.k@live.nl>
2021-05-09 20:51:09 +0200

    Correction

--- widget/xMLView/src/XMLView.tsx
+++ widget/xMLView/src/XMLView.tsx
@@ -1,15 +1,12 @@
 import { Component, ReactNode, createElement } from "react";
 import { HelloWorldSample } from "./components/HelloWorldSample";
-import { hot } from "react-hot-loader/root";
 
 import { XMLViewContainerProps } from "../typings/XMLViewProps";
 
 import "./ui/XMLView.css";
 
-class XMLView extends Component<XMLViewContainerProps> {
+export default class XMLView extends Component<XMLViewContainerProps> {
     render(): ReactNode {
         return <HelloWorldSample sampleText={this.props.sampleText ? this.props.sampleText : ""} />;
     }
 }
-
-export default hot(XMLView);
