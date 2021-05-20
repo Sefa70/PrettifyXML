64e7bdc026e9a8714d422150b1885a3f52718110
Sefa70 <Sefa.k@live.nl>
2021-05-20 13:36:50 +0200

    Update PrettifyXML.editorPreview.tsx

--- prettifyXML/src/PrettifyXML.editorPreview.tsx
+++ prettifyXML/src/PrettifyXML.editorPreview.tsx
@@ -1,43 +1,18 @@
 import { Component, ReactNode, createElement } from "react";
 import { PrettifyXMLPreviewProps } from "../typings/PrettifyXMLProps";
-import XMLViewer from 'react-xml-viewer';
 import "./ui/PrettifyXML.css";
+import XMLViewer from 'react-xml-viewer';
 
-declare function require(xml: string): string;
 
-export default class preview extends Component<PrettifyXMLPreviewProps> {
-        
-        render(): ReactNode {
-        const xml = this.props.textAttribute || "";
-        const customTheme = {
-        "attributeKeyColor"     :this.props.attributeKeyColor,
-        "attributeValueColor"   :this.props.attributeValueColor,
-        "cdataColor"            :this.props.cdataColor,
-        "commentColor"          :this.props.commentColor,
-        "separatorColor"        :this.props.separatorColor,
-        "tagColor"              :this.props.tagColor,
-        "textColor"             :this.props.textColor,
-        "overflowBreak"         :this.props.overflowBreak
-        }
-        const collapsible = {
-        "collapsible"           :this.props.collapsible
-        }
-  
-  return (<div className="style">
-        <XMLViewer
-        xml             = {xml}
-        theme           = {customTheme}
-        indentSize      = {this.props.indentSize}
-        invalidXml      = {this.props.invalidXml}
-        collapsible     = {collapsible}
-        style           = {this.props.style}
-        className       = {this.props.class}
-        
-        />
-        </div>);
-        }
+declare function require(name: string): string;
+
+export class preview extends Component<PrettifyXMLPreviewProps> {
+	render(): ReactNode {
+		const xml = `[${this.props.textAttribute}]`;
+		return <div><XMLViewer xml={xml} /></div>;
+	}
 }
 
-export function getPreviewCss(): string { 
-    return require("./ui/PrettifyXML.css");
+export function getPreviewCss(): string {
+	return require("./ui/PrettifyXML.css");
 }
