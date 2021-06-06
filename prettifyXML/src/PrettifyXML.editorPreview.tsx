b21858b03306472458540615f5f2b433e96c4f14
Sefa70 <Sefa.k@live.nl>
2021-06-06 12:01:55 +0200

    Feedback
    - code run with prettier,check with npm-run-release -> no errros
    - Name changed to com.timeseries
    - Classname changed to prettifyXML

--- prettifyXML/src/PrettifyXML.editorPreview.tsx
+++ prettifyXML/src/PrettifyXML.editorPreview.tsx
@@ -1,18 +1,21 @@
 import { Component, ReactNode, createElement } from "react";
 import { PrettifyXMLPreviewProps } from "../typings/PrettifyXMLProps";
 import "./ui/PrettifyXML.css";
-import XMLViewer from 'react-xml-viewer';
-
+import XMLViewer from "react-xml-viewer";
 
 declare function require(xml: string): string;
 
 export class preview extends Component<PrettifyXMLPreviewProps> {
-	render(): ReactNode {
-		const xml = "<Timeseries><Mendix>GoMakeIT</Mendix></Timeseries>";
-		return (<div><XMLViewer xml={xml} /></div>);
-	}
+    render(): ReactNode {
+        const xml = "<Timeseries><Mendix>GoMakeIT</Mendix></Timeseries>";
+        return (
+            <div>
+                <XMLViewer xml={xml} />
+            </div>
+        );
+    }
 }
 
 export function getPreviewCss(): string {
-	return require("./ui/PrettifyXML.css");
+    return require("./ui/PrettifyXML.css");
 }
--- prettifyXML/src/PrettifyXML.tsx
+++ prettifyXML/src/PrettifyXML.tsx
@@ -1,38 +1,39 @@
 import { Component, ReactNode, createElement } from "react";
 import { PrettifyXMLContainerProps } from "../typings/PrettifyXMLProps";
-import XMLViewer from 'react-xml-viewer';
+import XMLViewer from "react-xml-viewer";
 import "./ui/PrettifyXML.css";
 
-export default class Pretttify extends Component<PrettifyXMLContainerProps> {
-        
-        render(): ReactNode {
+export default class PrettifyXML extends Component<PrettifyXMLContainerProps> {
+    render(): ReactNode {
         const xml = this.props.textAttribute.value || "";
         const customTheme = {
-        "attributeKeyColor"     :this.props.attributeKeyColor,
-        "attributeValueColor"   :this.props.attributeValueColor,
-        "cdataColor"            :this.props.cdataColor,
-        "commentColor"          :this.props.commentColor,
-        "separatorColor"        :this.props.separatorColor,
-        "tagColor"              :this.props.tagColor,
-        "textColor"             :this.props.textColor,
-        "overflowBreak"         :this.props.overflowBreak
-        }
+            attributeKeyColor: this.props.attributeKeyColor,
+            attributeValueColor: this.props.attributeValueColor,
+            cdataColor: this.props.cdataColor,
+            commentColor: this.props.commentColor,
+            separatorColor: this.props.separatorColor,
+            tagColor: this.props.tagColor,
+            textColor: this.props.textColor,
+            overflowBreak: this.props.overflowBreak
+        };
         const collapsible = {
-        "collapsible"           :this.props.collapsible
-        }
-  
-  return (<div className="style">
-        <XMLViewer
-        xml             = {xml}
-        theme           = {customTheme}
-        indentSize      = {this.props.indentSize}
-        invalidXml      = {this.props.invalidXml}
-        collapsible     = {collapsible}
-        style           = {this.props.style}
-        className       = {this.props.class}
-        tabIndex        = {this.props.tabIndex}
-        disabled        = {this.props.textAttribute.readOnly}    
-        />
-        </div>);
-        }  
+            collapsible: this.props.collapsible
+        };
+
+        return (
+            <div className="style">
+                <XMLViewer
+                    xml={xml}
+                    theme={customTheme}
+                    indentSize={this.props.indentSize}
+                    invalidXml={this.props.invalidXml}
+                    collapsible={collapsible}
+                    style={this.props.style}
+                    className={this.props.class}
+                    tabIndex={this.props.tabIndex}
+                    disabled={this.props.textAttribute.readOnly}
+                />
+            </div>
+        );
+    }
 }
